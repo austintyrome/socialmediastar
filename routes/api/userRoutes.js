0785ef9d1830b8e6b9b33d5981dc88ab7638c839
austintyrome <austintyrome@gmail.com>
2024-07-15 17:42:42 -0600

    fixed api routes

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,24 @@
+const router = require('express').Router();
+const {
+  getUsers,
+  getSingleUser,
+  createUser,
+  updateUser,
+  deleteUser,
+  addReaction,
+  removeReaction,
+  addFriend,
+  deleteFriend,
+} = require('../../controllers/userController');
+
+router.route('/').get(getUsers).post(createUser);
+
+router.route('/:userId').get(getSingleUser).delete(deleteUser).put(updateUser);
+
+router.route('/:userId/reactions').post(addReaction);
+
+router.route('/:userId/reactions/:reactionId').delete(removeReaction);
+
+router.route('/:userId/friends/:friendId').post(addFriend).delete(deleteFriend);
+
+module.exports = router;
